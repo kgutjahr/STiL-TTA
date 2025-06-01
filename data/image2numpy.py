fc0842adb5baa9a08b42ed2ee3808ce83f02a250
Kevin Gutjahr <kevin.gutjahr@stud.uni-bamberg.de>
2025-06-01 21:50:27 +0200

    fix data jupyter files

--- data/image2numpy.py
+++ data/image2numpy.py
@@ -14,18 +14,8 @@ def process_DVM(
     DVM_feature_folder = '/mnt/data/kgutjahr/datasets/DVM/images'): 
     for split in ['train', 'val', 'test']:
         img_paths  = torch.load(os.path.join(DVM_feature_folder, '{}_paths_all_views.pt'.format(split)))
-        print('{}_paths_all_views.pt'.format(split))
-        new_paths = []
-        print(len(img_paths))
-        
-        for path in img_paths:
-            img_dir = os.path.dirname(path).replace(" ", "_")
-            img_filename = os.path.basename(path)
-            path = os.path.join(img_dir, img_filename)
-            new_paths.append(path)
-        
         np_paths = []
-        for path in tqdm(new_paths):
+        for path in tqdm(img_paths):
             img_np = plt.imread(path)
             save_path = path[:-4] + '.npy'
             np.save(save_path, img_np)
