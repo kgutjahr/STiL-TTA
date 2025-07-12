51871435dbec1ec6ef2d123a9e2e519f460672c0
Kevin Gutjahr <kevin.gutjahr@stud.uni-bamberg.de>
2025-07-12 15:25:48 +0200

    fix failed experiments

--- run_dist_shifts.sh
+++ run_dist_shifts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.1_black exp_name=black_0.1 evaluate=True
-
-CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.01_black exp_name=black_0.01 evaluate=True
-
-CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.1_b-s-b-g-w exp_name=b-s-b-g-w_0.1 evaluate=True
-
-CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.01_b-s-b-g-w exp_name=b-s-b-g-w_0.01 evaluate=True
+#CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.1_black exp_name=black_0.1 evaluate=True
+#
+#CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.01_black exp_name=black_0.01 evaluate=True
+#
+#CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.1_b-s-b-g-w exp_name=b-s-b-g-w_0.1 evaluate=True
+#
+#CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.01_b-s-b-g-w exp_name=b-s-b-g-w_0.01 evaluate=True
 
 CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.1_b-s-g exp_name=b-s-g_0.1 evaluate=True
 
@@ -16,6 +16,10 @@ CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dv
 
 CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.01_year exp_name=year_0.01 evaluate=True
 
-CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.1_miles exp_name=miles_0.1 evaluate=True
+#CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.1_miles exp_name=miles_0.1 evaluate=True
+#
+#CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.01_miles exp_name=miles_0.01 evaluate=True
 
-CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.01_miles exp_name=miles_0.01 evaluate=True
+CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.1_small exp_name=small_0.1 evaluate=True
+
+CUDA_VISIBLE_DEVICES=0 python -u run.py --config-name config_dvm_STiL dataset=dvm_all_server_reordered_SemiPseudo_0.01_small exp_name=small_0.01 evaluate=True
