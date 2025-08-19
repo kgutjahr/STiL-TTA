76eaa24d9e1f4c55a0a60b9bba9c2db385db8549
Kevin Gutjahr <kevin.gutjahr@stud.uni-bamberg.de>
2025-08-19 22:50:01 +0200

    yet another fix

--- run_dist_shifts.sh
+++ run_dist_shifts.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 
-# Define parameters
-DATASETS=("black" "miles" "normal")
-MODALITIES=("tabular" "image" "multi")
-#AUGS=("delta" "extrapolation" "mixstyle" "noise")
-AUGS=("noise")
-
 # Create log directory if it doesn't exist
 mkdir -p error_logs
 
-# Loop over all combinations
-for ds in "${DATASETS[@]}"; do
-  for mod in "${MODALITIES[@]}"; do
-    for aug in "${AUGS[@]}"; do
+run_experiment () {
+  local DATASETS=("${!1}")
+  local MODALITIES=("${!2}")
+  local AUGS=("${!3}")
+  local TAG=$4
 
-      CONFIG="config_dvm_STiL_input_nothing_latent_${mod}_${aug}"
-      EXP="latent_augmentation_results/latent_only_before_classifier_whole/${ds}_${mod}_${aug}"
-      LOG="error_logs/${ds}_${mod}_${aug}.log"
+  for ds in "${DATASETS[@]}"; do
+    for mod in "${MODALITIES[@]}"; do
+      for aug in "${AUGS[@]}"; do
 
-      echo ">>> Running: dataset=$ds, modality=$mod, aug=$aug"
-      CUDA_VISIBLE_DEVICES=1 python -u run.py \
-        --config-name "$CONFIG" \
-        dataset="shifted_configs/dvm_all_server_reordered_SemiPseudo_0.1_${ds}" \
-        exp_name="$EXP" \
-        evaluate=True \
-        2> "$LOG"
-      echo ">>> Finished: dataset=$ds, modality=$mod, aug=$aug"
+        CONFIG="config_dvm_STiL_input_nothing_latent_${mod}_${aug}"
+        EXP="latent_augmentation_results/latent_only_before_classifier_whole/${ds}_${mod}_${aug}"
+        LOG="error_logs/${ds}_${mod}_${aug}.log"
 
+        echo ">>> Running $TAG: dataset=$ds, modality=$mod, aug=$aug"
+        CUDA_VISIBLE_DEVICES=1 python -u run.py \
+          --config-name "$CONFIG" \
+          dataset="shifted_configs/dvm_all_server_reordered_SemiPseudo_0.1_${ds}" \
+          exp_name="$EXP" \
+          evaluate=True \
+          2> "$LOG"
+        echo ">>> Finished $TAG: dataset=$ds, modality=$mod, aug=$aug"
+
+      done
     done
   done
-done
+}
+
+# Experiment 1
+DATASETS1=("black" "miles" "normal")
+MODALITIES1=("image")
+AUGS1=("noise")
+run_experiment DATASETS1[@] MODALITIES1[@] AUGS1[@] "EXP1"
+
+
+# Experiment 2
+DATASETS2=("all")
+MODALITIES2=("tabular" "image" "multi")
+AUGS2=("delta" "extrapolation" "mixstyle" "noise")
+run_experiment DATASETS2[@] MODALITIES2[@] AUGS2[@] "EXP2"
