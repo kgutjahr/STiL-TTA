aa785fecd93af0a8a70fd9d1eef54ebe09cb0653
Kevin Gutjahr <kevin.gutjahr@stud.uni-bamberg.de>
2025-11-03 18:09:36 +0100

    better runs

--- run_dist_shifts.sh
+++ run_dist_shifts.sh
@@ -7,26 +7,43 @@ run_experiment () {
   local CONFIGS=("${!1}")
   local DATASETS=("${!2}")
   local PRETRAIN=("${!3}")
-  local DEVICE=$4
-  local REPEAT=$5
-  local RESULT_DIR=$6
+  local BATCHSIZES=("${!4}")
+  local DEVICE=$5
+  local REPEAT=$6
+  local RESULT_DIR=$7
+  declare -n PARAMS_REF=$8
 
   for ((i=1; i<=$REPEAT; i++)); do
     for con in "${CONFIGS[@]}"; do
       for ds in "${DATASETS[@]}"; do
-        CONFIG="${con}"
-        EXP="${RESULT_DIR}/${con}_${ds}"
-        LOG="error_logs/${ds}.log"
-        #
-        echo ">>> Running: dataset=$ds, modality=$con"
-        CUDA_VISIBLE_DEVICES=$DEVICE python -u run.py \
-          --config-name "$CONFIG" \
-          dataset="shifted_configs/ADNI/adni_${ds}" \
-          exp_name="$EXP" \
-          evaluate=True \
-          pretrain="${PRETRAIN[@]}"
-          2> "$LOG"
-        echo ">>> Finished: dataset=$ds, modality=$con"
+        for b in "${BATCHSIZES[@]}"; do
+          CONFIG="${con}"
+
+          if (( ${#BATCHSIZES[@]} > 1 )); then
+            EXP="${RESULT_DIR}/${con}_${ds}_${b}"
+          else
+            EXP="${RESULT_DIR}/${con}_${ds}"
+          fi
+
+          LOG="error_logs/${ds}_${b}.log"
+
+          EXTRA_ARGS=""
+          for key in "${!PARAMS_REF[@]}"; do
+            EXTRA_ARGS+=" ${key}=${PARAMS_REF[$key]}"
+          done
+
+          #
+          echo ">>> Running: dataset=$ds, modality=$con"
+          CUDA_VISIBLE_DEVICES=$DEVICE python -u run.py \
+            --config-name "$CONFIG" \
+            dataset="shifted_configs/${ds}" \
+            exp_name="$EXP" \
+            evaluate=True \
+            pretrain="${PRETRAIN[@]}" \
+            $EXTRA_ARGS \
+            2> "$LOG"
+          echo ">>> Finished: dataset=$ds, modality=$con"
+        done
       done
     done
   done
@@ -34,11 +51,18 @@ run_experiment () {
 #
 
 # Experiment
-CONFIGS=("config_dvm_STiL_MoE_0.1_1")
-DATASETS=("normal" "weight" "age" "TE")
+CONFIGS=("config_dvm_STiL_consent_0.1")
+DATASETS=("configs/augment_configs/dataset/shifted_configs/TIP/dvm_all_server_reordered_SemiPseudo_TIP_normal" "configs/augment_configs/dataset/shifted_configs/TIP/dvm_all_server_reordered_SemiPseudo_TIP_black" "configs/augment_configs/dataset/shifted_configs/TIP/dvm_all_server_reordered_SemiPseudo_TIP_miles" "configs/augment_configs/dataset/shifted_configs/TIP/dvm_all_server_reordered_SemiPseudo_TIP_color_miles")
+BATCHSIZES=(512)
 DEVICE=1
 REPEAT=10
-RESULT_DIR="ADNI/MoE_1_cut"
+RESULT_DIR="ADNI/final_dataset/baseline"
+declare -A EXTRA_PARAMS=(
+  ["train_logit_consent"]=False
+  ["cut_classifier_input"]=False
+  ["replace_ce_loss"]=False
+)
+
 
 PRETRAIN=(FALSE)
-run_experiment CONFIGS[@] DATASETS[@] PRETRAIN[@] $DEVICE $REPEAT $RESULT_DIR
+run_experiment CONFIGS[@] DATASETS[@] PRETRAIN[@] BATCHSIZES[@] $DEVICE $REPEAT $RESULT_DIR EXTRA_PARAMS
